9cb0ea7f85d10a4cd69d1075fb749f64a8f26014
david oh <david.oh@autodesk.com>
2018-08-23 22:16:39 -0700

    Clean up code style per pycodestyle, pydoc style, and pylint

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,53 +1,78 @@
+#!/usr/bin/python    : #! interpreter
+# -*- coding: utf-8 -*-       # encoding?
+
+"""Webotron: Deploy websites with AWS.
+
+Webotro automates the process of deploying static websites to AWS.
+- configure AWS S3 list_buckets
+    - Create them
+    - Set them up for statis website hosting
+    - Deploy local files to them
+- configure DNS with AWS Route 53
+- Configure a content delivery Network and SSL with AWS
+
+## it's a good habit to leave the comments in the doc string
+"""
+
+from pathlib import Path
+import mimetypes
+from botocore.exceptions import ClientError
 import boto3
 import sys
 import click
-import mimetypes
-from botocore.exceptions import ClientError
-from pathlib import Path
 
-session = boto3.Session(profile_name = 'pythonAutomation')
-s3 = session.resource('s3')
+
+SESSION=boto3.Session(profile_name='pythonAutomation')
+S3=SESSION.resource('s3')
+
 
 @click.group()
 def cli():
-    "Webotron deploys websites to AWS"
+    """Webotron deploys websites to AWS."""
     pass
+
 
 #@click.command('list-buckets')
 @cli.command('list-buckets') # cli becomes the decorator. deligate control of command
 # @click is a decorator. and it will control the way our function runs
 def list_buckets():
-    "List all s3 buckets" # document string. use --help
-    for bucket in s3.buckets.all():
+    """List all S3 buckets.""" # document string. use --help
+    for bucket in S3.buckets.all():
         print(bucket)
+
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    "List contents of the bucket"
-    for object in s3.Bucket(bucket).objects.all():
+    """List contents of the bucket."""
+    for object in S3.Bucket(bucket).objects.all():
         print(object)
+
 
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-    "Create and configure S3 bucket"
+    """Create and configure S3 bucket."""
     print("requesting to create bucket with name "+bucket)
     # has to be defined before going into try and except block
-    s3_bucket = None
+    s3_bucket=None
 
     # s3_bucket = s3.create_bucket(
     #     Bucket= bucket,
     #     CreateBucketConfiguration={'LocationConstraint' : session.region_name})
 
-    try: s3_bucket = s3.create_bucket(
-            Bucket= bucket,
-            CreateBucketConfiguration={'LocationConstraint' : session.region_name})
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
-            s3_bucket = s3.Bucket(bucket)
+    try: s3_bucket = S3.create_bucket(
+        Bucket= bucket,
+        CreateBucketConfiguration={
+            'LocationConstraint' : SESSION.region_name
+            }
+        )
+
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = S3.Bucket(bucket)
         else:
-            raise e
+            raise error
 
     policy = """
     {
@@ -80,7 +105,9 @@ def setup_bucket(bucket):
     )
     return
 
+
 def upload_file(s3_bucket, path, key):
+    """Upload file to S3 bucket."""
     content_type = mimetypes.guess_type(key)[0] or 'text/plain'
     s3_bucket.upload_file(
         path,
@@ -90,24 +117,27 @@ def upload_file(s3_bucket, path, key):
         }
     )
 
+
 @cli.command('sync')
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
 def syn(pathname, bucket):
-    "Sync Contents of PATHNAME to BUCKET"
-    s3_bucket = s3.Bucket(bucket)
+    """Sync Contents of PATHNAME to BUCKET."""
+    s3_bucket = S3.Bucket(bucket)
 
     #closure
     root = Path(pathname).expanduser().resolve()
 
-    # the pathname given is relative path. expanduser expands this to absolute path. and resolve... what does it do?
+    # the pathname given is relative path. expanduser expands this to absolute path.
+    # and resolve... what does it do?
     # both full path / relative path / tilda all works
     def handle_directory(target):
         for p in target.iterdir():
-            if p.is_dir(): handle_directory(p)
-            if p.is_file() : upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+            if p.is_dir():
+                handle_directory(p)
+            if p.is_file():
+                upload_file(s3_bucket, str(p), str(p.relative_to(root)))
                                 # print("Path : {}\n Key : {}".format(p, p.relative_to(root)))
-
 
     handle_directory(root)
 
@@ -121,5 +151,5 @@ if __name__ == '__main__':
     cli()
     print("I am using click and click.group 2")
 
-## python convention
-## when this file run as script __name__ will eaqual __main__
+# python convention
+# when this file run as script __name__ will eaqual __main__
